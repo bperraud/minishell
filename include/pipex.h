cb5548117aaefab065aad8bcdaff2c71c135573d
bperraud <74benji@live.fr>
2022-04-21 01:44:40 +0200

    refactoring

--- include/pipex.h
+++ include/pipex.h
@@ -6,7 +6,7 @@
 /*   By: bperraud <bperraud@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/19 18:34:43 by bperraud          #+#    #+#             */
-/*   Updated: 2022/04/20 00:11:41 by bperraud         ###   ########.fr       */
+/*   Updated: 2022/04/21 01:42:54 by bperraud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@
 //pipex
 void	multiple_cmd(int fd[3], int argc, char **argv, char **envp);
 int		here_doc(char *limiter);
-void	pipex_alone(char *arg, char**envp);
+void	pipex(char *arg, char**envp);
 void	exec_cmd(char *arg, char **envp);
 
 //files
@@ -34,12 +34,6 @@ char	*create_path(char *path, char *arg);
 int		open_file(char *file);
 
 // utils
-char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
-int		ft_strncmp(const char *s1, const char *s2, size_t n);
-char	*ft_strdup(const char *s1);
-size_t	ft_strlen(const char *s);
-void	ft_putstr_fd(char *s, int fd);
 char	*ft_strncpy_split(char *dest, const char *src, unsigned int n);
 void	exit_error(char **cmd1, char **cmd2, char **path);
 void	free_all(char **cmd1, char **cmd2, char **path);
