3e270eeddab97686495fa13793b55e3e5fde1881
bperraud <74benji@live.fr>
2022-06-05 18:17:04 +0200

    norme

--- src/exec/subshell.c
+++ src/exec/subshell.c
@@ -12,7 +12,7 @@
 
 #include "minishell.h"
 
-static char *cmd_join(t_cmd *command)
+static char	*cmd_join(t_cmd *command)
 {
 	int		argc;
 	char	*tmp;
@@ -56,41 +56,3 @@ void	subshell(t_cmd *command, char **envp)
 	free(full_path);
 	free_str_list(sh);
 }
-
-
-/*
-void	subshell_pipe(t_cmd *command, char **envp)
-{
-	char	**sh;
-
-	sh = smalloc(4 * sizeof(char *));
-	sh[0] = ft_sstrdup("./minishell");
-	sh[1] = ft_sstrdup("-c");
-	sh[2] = ft_sstrdup(command->cmd[0]);
-	sh[3] = NULL;
-
-
-
-	ft_executable(sh, envp);
-	free_str_list(sh);
-}
-
-void	subshell(t_cmd *cmd, char **envp)
-{
-	char	**sh;
-	char	*pwd;
-	char	*full_path;
-
-	pwd = ft_getenv("PWD", envp);
-	full_path = ft_strjoin(pwd, "/minishell");
-	free(pwd);
-	sh = smalloc(4 * sizeof(char *));
-	sh[0] = ft_sstrdup(full_path);
-	sh[1] = ft_sstrdup("-c");
-	sh[2] = ft_strndup(cmd->cmd[0] + 1, ft_strlen(cmd->cmd[0]) - 2);
-	sh[3] = NULL;
-	ft_executable(sh, envp);
-	free(full_path);
-	free_str_list(sh);
-}
-*/
