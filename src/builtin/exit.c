1dc5d3a3fb772eabd3afe8d2219431d553107cf5
Benjamin Perraudin <bperraud@fu-r4-p5.s19.be>
2022-06-11 15:44:26 +0200

    end

--- src/builtin/exit.c
+++ src/builtin/exit.c
@@ -17,6 +17,25 @@ void	ft_exit(t_cmd *cmd)
 	cmd->fd_in = EXIT_CODE;
 }
 
+static bool	has_no_digit(char *str)
+{
+	while (*str)
+	{
+		if (!ft_isdigit(*str))
+			return (false);
+		str++;
+	}
+	return (true);
+}
+
+static void	print_exit(char *str)
+{
+	ft_putstr_fd("-minishell: exit: ", 1);
+	ft_putstr_fd(str, 1);
+	ft_putstr_fd(": numeric argument required\n", 1);
+	g_error = 255;
+}
+
 void	try_exit(t_cmd *command, char *str, char **envp)
 {
 	int	arg;
@@ -31,11 +50,16 @@ void	try_exit(t_cmd *command, char *str, char **envp)
 		prompt_error("exit", "too many arguments\n");
 		return ;
 	}
+	ft_putstr_fd("exit\n", 1);
 	if (arg == 2)
-		g_error = ft_atoi(command->cmd[1]);
+	{
+		if (!has_no_digit(command->cmd[1]))
+			print_exit(command->cmd[1]);
+		else
+			g_error = ft_atoi(command->cmd[1]);
+	}
 	free_t_cmd(command);
 	free(str);
 	free_str_list(envp);
-	ft_putstr_fd("exit\n", 1);
 	exit(g_error);
 }
